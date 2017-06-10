95392c26498df27ff2138051d2b4b1e6fd8de5b7
Everton Silva <thonns@gmail.com>
2017-06-10 20:01:58 -0300

    Feature: add embedded H2 database to project

---
+++ src/main/java/br/com/everton/persistence/ConnectionFactory.java
@@ -0,0 +1,19 @@
+package br.com.everton.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by everton on 10/06/17.
+ */
+public class ConnectionFactory {
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:h2:~/tmp/data/sistemacomandas");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
