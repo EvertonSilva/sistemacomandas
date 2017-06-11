9c0174278ee231e10e9f288c4a6ca9aa6758913f
Everton Silva <thonns@gmail.com>
2017-06-11 01:58:47 -0300

    Feature: create Order class and OrderDao class

---
+++ src/main/java/br/com/everton/persistence/dao/OrderDao.java
@@ -0,0 +1,41 @@
+package br.com.everton.persistence.dao;
+
+import br.com.everton.model.Order;
+import br.com.everton.persistence.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Calendar;
+
+/**
+ * Created by everton on 11/06/17.
+ */
+public class OrderDao {
+    private Connection conn;
+
+    public OrderDao() {
+        this.conn = new ConnectionFactory().getConnection();
+    }
+
+    public void inserir(Order order) {
+        // sql string
+        String sql = "insert into orders " +
+                "(total, dateOrdered, createdAt, updatedAt)" +
+                "values (?,?,?,?)";
+
+        try {
+            // statment to run query
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            // set values
+            stmt.setDouble(1, order.getTotal());
+            stmt.setDate(2, new Date(order.getOrderedDate().getTimeInMillis()));
+            stmt.setDate(3, new Date(Calendar.getInstance().getTimeInMillis()));
+            stmt.setDate(4, new Date(Calendar.getInstance().getTimeInMillis()));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
