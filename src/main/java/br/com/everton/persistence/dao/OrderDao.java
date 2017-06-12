cb8abb42f1540a8646bc1f4800d54a5a6c5a5dbb
Everton Silva <thonns@gmail.com>
2017-06-11 22:26:36 -0300

    Feature: list Orders from database

--- src/main/java/br/com/everton/persistence/dao/OrderDao.java
+++ src/main/java/br/com/everton/persistence/dao/OrderDao.java
@@ -3,11 +3,10 @@ package br.com.everton.persistence.dao;
 import br.com.everton.model.Order;
 import br.com.everton.persistence.ConnectionFactory;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by everton on 11/06/17.
@@ -17,6 +16,14 @@ public class OrderDao {
 
     public OrderDao() {
         this.conn = new ConnectionFactory().getConnection();
+    }
+
+    public void closeConn() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void insert(Order order) {
@@ -38,6 +45,34 @@ public class OrderDao {
             // execute query
             stmt.execute();
             stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Order> getList() {
+        try {
+            PreparedStatement stmt = conn.prepareStatement("select * from orders");
+            ResultSet result = stmt.executeQuery();
+            List<Order> orders = new ArrayList<>();
+            Calendar date = Calendar.getInstance();
+
+            while(result.next()) {
+                Order order = new Order();
+                // set date
+                date.setTime(result.getDate("dateOrdered"));
+                order.setOrderedDate(date);
+
+                // set total
+                order.setTotal(result.getDouble("total"));
+
+                orders.add(order);
+            }
+
+            result.close();
+            stmt.close();
+
+            return orders;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
