3517ba1c277410924e06aecf9a9f36124a37c008
Everton Silva <thonns@gmail.com>
2017-06-21 02:26:27 -0300

    Refactor: add getters/setters to Product class

--- src/main/java/br/com/everton/model/order/Product.java
+++ src/main/java/br/com/everton/model/order/Product.java
@@ -1,5 +1,7 @@
 package br.com.everton.model.order;
 
+import java.math.BigDecimal;
+
 /**
  * This class represents a industrialized product
  *
@@ -14,11 +16,35 @@ public class Product  extends Merchandise {
     public Product() {
     }
 
+    public Long getId() {
+        return super.id;
+    }
+
+    public void setId(Long id) {
+        super.id = id;
+    }
+
     public String getBarcode() {
         return barcode;
     }
 
     public void setBarcode(String barcode) {
         this.barcode = barcode;
+    }
+
+    public String getName() {
+        return super.name;
+    }
+
+    public void setName(String name) {
+        super.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return super.price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        super.price = price;
     }
 }
