9fa99453887e71ac99a0479f240ad8edb22df216
Everton Silva <thonns@gmail.com>
2017-06-21 01:23:50 -0300

    Chore: Create migrations to add some sample data

---
+++ src/main/resources/db/migration/V3__Add_products.sql
@@ -0,0 +1,19 @@
+-- Add some products to working with
+
+INSERT INTO products (id, name, description, price, created_at, updated_at)
+VALUES (1, 'Refeição', 'Prato por quilo', 25.0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+INSERT INTO products (id, name, description, price, created_at, updated_at)
+VALUES (2, 'Suco natural', 'Sucos de frutas preparados na hora', 3.5, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+INSERT INTO products (id, name, description, price, created_at, updated_at)
+VALUES (3, 'Sobremesa', '', 15.0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+INSERT INTO products (id, name, description, price, created_at, updated_at)
+VALUES (4, 'Refrigerante Lata', '', 3.5, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+INSERT INTO products (id, name, description, price, created_at, updated_at)
+VALUES (5, 'Coxinha', '', 2.75, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+INSERT INTO products (id, name, description, price, created_at, updated_at)
+VALUES (6, 'Sanduíche Natural', '', 5.0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
---
+++ src/main/resources/db/migration/V4__Add_order_cards.sql
@@ -0,0 +1,12 @@
+-- Add some Orders Card to work with
+
+INSERT INTO order_cards (id, barcode) VALUES (1, '0001');
+INSERT INTO order_cards (id, barcode) VALUES (2, '0002');
+INSERT INTO order_cards (id, barcode) VALUES (3, '0003');
+INSERT INTO order_cards (id, barcode) VALUES (4, '0004');
+INSERT INTO order_cards (id, barcode) VALUES (5, '0005');
+INSERT INTO order_cards (id, barcode) VALUES (6, '0006');
+INSERT INTO order_cards (id, barcode) VALUES (7, '0007');
+INSERT INTO order_cards (id, barcode) VALUES (8, '0008');
+INSERT INTO order_cards (id, barcode) VALUES (9, '0009');
+INSERT INTO order_cards (id, barcode) VALUES (10, '0010');
